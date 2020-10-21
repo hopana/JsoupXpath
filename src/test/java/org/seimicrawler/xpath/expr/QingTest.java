f60be151e5273b82e81530d5e2f615212448a2cb
HuPan <hupan1@meizu.com>
2020-10-21 14:17:03 +0800

    一些API测试用例

--- src/test/java/org/seimicrawler/xpath/expr/QingTest.java
+++ src/test/java/org/seimicrawler/xpath/expr/QingTest.java
@@ -1,28 +1,14 @@
 package org.seimicrawler.xpath.expr;
 
-import com.sun.tools.hat.internal.model.Root;
-import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.ParseTree;
 import org.apache.commons.io.FileUtils;
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.seimicrawler.xpath.BaseTest;
-import org.seimicrawler.xpath.antlr.XpathLexer;
-import org.seimicrawler.xpath.antlr.XpathParser;
-import org.seimicrawler.xpath.core.XValue;
-import org.seimicrawler.xpath.core.XpathProcessor;
-import org.seimicrawler.xpath.exception.DoFailOnErrorHandler;
 
 import java.io.File;
-import java.math.BigDecimal;
 import java.net.URL;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 /**
@@ -35,7 +21,7 @@ public class QingTest extends BaseTest {
     @Test
     public void imgTest1() throws Exception {
         ClassLoader loader = getClass().getClassLoader();
-        URL url = loader.getResource("qing.html");
+        URL url = loader.getResource("qing1.html");
         assert url != null;
         File contentFile = new File(url.toURI());
         String context = FileUtils.readFileToString(contentFile, StandardCharsets.UTF_8);
@@ -61,6 +47,49 @@ public class QingTest extends BaseTest {
         img.attr("test", "hello world");
 
         System.out.println(root.toString());
+    }
+
+
+    @Test
+    public void imgTest3() throws Exception {
+        String FILE_DOMAIN = "https://ssm.res.meizu.com/";
+
+        ClassLoader loader = getClass().getClassLoader();
+        URL url = loader.getResource("qing3.html");
+        assert url != null;
+        File contentFile = new File(url.toURI());
+        String context = FileUtils.readFileToString(contentFile, StandardCharsets.UTF_8);
+        Elements root = Jsoup.parse(context).children();
+
+        Elements imgs = root.select("img");
+        for (Element img : imgs) {
+            String tempSrc = img.attr("src");
+            String finalSrc = FILE_DOMAIN + tempSrc.replace("temp", "content");
+            img.attr("src", finalSrc);
+        }
+
+        System.out.println(root.toString());
+    }
+
+
+    @Test
+    public void imgTest4() throws Exception {
+        String FILE_DOMAIN = "https://ssm.res.meizu.com/";
+        String content = "<p>gghhhh</p><img src=\"temp/2020/10/21/76467/1603249482288.jpg\"/><p>hhhh</p>";
+
+
+        Elements root = Jsoup.parse(content).children();
+        Elements elements = root.select("img");
+
+        if (elements.size() > 0) {
+            for (Element img : elements) {
+                String tempSrc = img.attr("src");
+                String finalSrc = FILE_DOMAIN + tempSrc.replace("temp", "content");
+                img.attr("src", finalSrc);
+            }
+        }
+
+        System.out.println(root.select("body").html());
     }
 
 }
